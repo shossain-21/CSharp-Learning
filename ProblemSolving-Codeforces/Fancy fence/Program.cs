f591586550f59fb4e468b966d7c3d336ec48b766
Sanzida Hossain <sanzidahossain.dev@gmail.com>
2025-01-05 13:00:18 +0600

    Problem Solved

---
+++ ProblemSolving-Codeforces/Fancy fence/Program.cs
@@ -0,0 +1,22 @@
+﻿int t = int.Parse(Console.ReadLine());
+
+while(t > 0)
+{
+    int a = int.Parse(Console.ReadLine());
+    int flag = 0;
+    for(int i = 3; i < 361; i++)
+    {
+        int x = (i-2) * 180;
+        if (x == a*i)
+        {
+            Console.WriteLine("YES");
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+    {
+        Console.WriteLine("NO");
+    }
+    t--;
+}
---
+++ ProblemSolving-Codeforces/Vanya and Lanterns/Program.cs
@@ -0,0 +1,18 @@
+﻿string input = Console.ReadLine();
+int n = int.Parse(input.Split(' ')[0]);
+int l = int.Parse(input.Split(' ')[1]);
+int[] lanterns = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+Array.Sort(lanterns);
+double maxDistance = 0;
+for (int i = 0; i < n - 1; i++)
+{
+    double distance = (lanterns[i + 1] - lanterns[i]) / 2.0;
+    if (distance > maxDistance)
+    {
+        maxDistance = distance;
+    }
+}
+double distanceToStart = lanterns[0];
+double distanceToEnd = l - lanterns[n - 1];
+double result = Math.Max(maxDistance, Math.Max(distanceToStart, distanceToEnd));
+Console.WriteLine($"{result:F10}");
